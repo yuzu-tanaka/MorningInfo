77740ce814d4d11e41c6847faa82baea00f1b9b4
yuzu tanaka <else.something@gmail.com>
2020-11-13 01:00:18 +0900

    ファイル整理

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Start MorningInfo"
-cd /home/pi/Develop/MornigInfo
-
-python3 displayOnOff.py &
-python3 app.py &
-sleep 15s
-chromium-browser --kiosk --incognito "http://0.0.0.0:5000" &
